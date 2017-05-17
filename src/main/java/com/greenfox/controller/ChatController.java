a90c29cbc2e8fbccd908b74f9c28c9eb4a6d770b
Reka Giday <giday.r@gmail.com>
2017-05-17 14:41:21 +0200

    add mainpage html and register endpoint

--- src/main/java/com/greenfox/controller/ChatController.java
+++ src/main/java/com/greenfox/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.greenfox.controller;
 import com.greenfox.logging.RequestLogger;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +14,25 @@ public class ChatController {
   RequestLogger requestLogger;
 
 
-  @RequestMapping(value = "/")
+  @GetMapping(value = "/")
   public String home(HttpServletRequest request) {
     String welcomeMessage = "";
     try {
-      welcomeMessage = "Peer to Peer chat program";
+      welcomeMessage = "Peer to Peer chat program 1";
       requestLogger.info(request);
     } catch (Exception e) {
       requestLogger.error(request);
     }
     return welcomeMessage;
   }
+
+  @GetMapping(value = "/register")
+  public String register() {
+    return "register";
+  }
+
+
+
+
+
 }
