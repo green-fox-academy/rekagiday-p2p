2144469758e38bf93e5984b8eecf5bdfcde4140e
Reka Giday <giday.r@gmail.com>
2017-05-18 13:43:08 +0200

    rebuilding

--- src/main/java/com/greenfox/model/Post.java
+++ src/main/java/com/greenfox/model/Post.java
@@ -1,6 +1,7 @@
 package com.greenfox.model;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ public class Post {
 
 
   @Id
-  @GeneratedValue (strategy = )
+  @GeneratedValue (strategy = GenerationType.AUTO)
   int id;
 
 }
